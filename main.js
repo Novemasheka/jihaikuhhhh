5bbc89521eb2e4955b2df50ac49d2ec2f51125f3
Novemasheka <vemasgamer2@gmail.com>
2025-10-26 18:06:10 +0700

    last

--- main.js
+++ main.js
@@ -4,21 +4,34 @@ let btn = document.getElementById("submit");
 let ucapan = document.getElementById("ucapan");
 let send = document.getElementById("kirimkan")
 let lagu = document.getElementById("lagu");
+let musik= [document.getElementById("mati"),document.getElementById("hidup")]
 window.onscroll = function() {Headers()}
 
 ucapan.addEventListener("click", function Headers() {
     
     alert('🎉 Selamat ulang tahun Jihaikuhh! \n Meskipun Hadiah yang ku berikan tidak seberapa :), \n Bisa jadi ini adalah hadiah terakhir dari aku \nSemoga suka yah ^^ ❤️❤️❤️\n\n Tekan "OK" untuk mengirim pesan buat Novell :D')
     lagu.play();
+    musik[0].style.display = "block"
     notif[0].style.display = "block";
     notif[0].style.animation = "fadeIn 1s ease-in-out";
+    confetti({
+          particleCount: 120,
+          spread: 90,
+          origin: { y: 0.6 }
+        });
     document.documentElement.scrollTop = 1000
     document.body.scrollTop = 1000
 })
 
 function matikan() {
   lagu.pause();
-  lagu.currentTime = 0;
+  musik[0].style.display = "none"
+  musik[1].style.display = "block"
+}
+function hidupkan() {
+  lagu.play();
+  musik[0].style.display = "block"
+  musik[1].style.display = "none"
 }
 
     
